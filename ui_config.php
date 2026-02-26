53e94722779679425a02fac4c3e4c1c52d7bd5ce
dndraamy <dndramylza12@gmail.com>
2026-02-26 12:30:52 +0700

    fix: rename config.php to ui_config.php

---
+++ ui_config.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="style/color.css">
+
+<script src="https://cdn.tailwindcss.com"></script>
+
+<script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          utama: 'var(--color-primary)',
+          gelap: 'var(--color-primary-dark)',
+          sukses: 'var(--color-success)',
+          latar: 'var(--color-background)',
+          aksen: 'var(--color-accent)',
+        }
+      }
+    }
+  }
+</script>
